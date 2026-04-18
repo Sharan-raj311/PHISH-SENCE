0f6ca807ce6cdb21698f89dfd32d27c678bd2bfa
Sharan-raj311 <dasasharan311@gmail.com>
2026-04-18 17:13:34 +0530

    Delete popup.js

--- popup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const proBtn = document.getElementById("proBtn");
-
-    proBtn.addEventListener("click", function() {
-        chrome.tabs.create({
-            url: "https://www.virustotal.com/gui/home/url"
-        });
-    });
-});
